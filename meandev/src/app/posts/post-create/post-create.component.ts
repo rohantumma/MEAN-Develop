1ccfe0454112fbc1904038bd1bdfe59ae85885ac
rohantumma <rohantumma98@gmail.com>
2020-10-27 12:00:25 +0530

    conponent craete

---
+++ meandev/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,11 @@
+import { from } from 'rxjs';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+
+})
+export class PostCreateComponent {
+
+}
