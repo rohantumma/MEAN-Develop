44addc978f769fcc49682da7204ef958507c0bf2
rohantumma <rohantumma98@gmail.com>
2020-10-27 17:02:48 +0530

    Post List

--- meandev/src/app/app.module.ts
+++ meandev/src/app/app.module.ts
@@ -3,7 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatInputModule, MatCardModule } from "@angular/material";
+import { MatInputModule,
+         MatCardModule,
+         MatToolbarModule,
+         MatExpansionModule
+
+       } from "@angular/material";
+import { MatButtonModule} from "@angular/material";
 
 
 
@@ -12,12 +18,16 @@ import { AppComponent } from './app.component';
 
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { from } from 'rxjs';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +35,10 @@ import { from } from 'rxjs';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ meandev/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,19 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent{
+  posts = [
+
+    { title: 'First Post', content: 'This is 1' },
+    { title: 'Second Post', content: 'This is 2' },
+    { title: 'Third Post', content: 'This is 3' },
+
+  ];
+
+
+}
