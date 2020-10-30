3da749b4951c48bab0b0929283ff508ebf5af4ef
rohantumma <rohantumma98@gmail.com>
2020-10-30 16:10:41 +0530

    Routing done

--- meandev/src/app/app-routing.module.ts
+++ meandev/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {path: '', component:PostListComponent},
+  {path: 'create', component:PostCreateComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
